4dcc813bcead034b2dbc563913495d84912fcf7b
Rayen-panda <chebbirayen1@gmail.com>
2022-10-13 23:06:51 +0200

    first commit

---
+++ src/Data.js
@@ -0,0 +1,62 @@
+export const Data = [
+    {
+        id: 1,
+        title: "MR.ROBOT",
+        rate: 4,
+        description:
+            "Elliot Alderson est un jeune informaticien vivant à New York, qui travaille en tant qu'ingénieur en sécurité informatique pour Allsafe Security. Celui-ci luttant constamment contre un trouble d'anxiété sociale et de dépression..",
+        posterUrl:
+            "https://m.media-amazon.com/images/M/MV5BM2QyNDIzOGMtNThhNS00NmUwLWI0ZjUtZjdkN2I1OTRjZWQ3XkEyXkFqcGdeQXVyNzQ1ODk3MTQ@._V1_.jpg",
+        traileriframe:'https://youtu.be/xIBiJ_SzJTA'
+    },
+
+    {
+        id: 2,
+        title: "Avengers: Endgame",
+        rate: 1,
+        description:
+            "After the devastating events of Avengers: Infinity War (2018), the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos' actions and restore balance to the universe.",
+        posterUrl:
+            "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_.jpg",
+        traileriframe:'https://www.youtube.com/embed/dJTU48_yghs'
+    },
+    {
+        id: 3,
+        title: "Noura rêve",
+        rate: 3,
+        description:
+            "Noura rêve est un film dramatique franco-belgo-tunisien réalisé par Hinde Boujemaa et sorti en 2019. Lors des Journées cinématographiques de Carthage 2019, il remporte le Tanit d'or et le Tanit de la meilleure interprétation féminine pour Hend Sabri.",
+        posterUrl:"https://www.avoir-alire.com/local/cache-vignettes/L672xH911/1884265.jpg-r_1920_1080-f_jpg-q_x-xxyxx-a75a5.jpg?1578283496",
+        traileriframe:"https://youtu.be/3nh5WhTRsls"
+    },
+    {
+        id: 4,
+        title: "baby driver",
+        rate: 3,
+        description:
+            "Chauffeur pour des braqueurs de banque, Baby a un truc pour être le meilleur dans sa partie : il roule au rythme de sa propre playlist. Lorsqu'il rencontre la fille de ses rêves, Baby cherche à mettre fin à ses activités criminelles pour revenir dans le droit chemin. Il est forcé de travailler pour un grand patron du crime et le braquage tourne mal. Désormais, sa liberté, son avenir avec la fille qu'il aime et sa vie sont en jeu",
+        posterUrl:
+            "https://m.media-amazon.com/images/M/MV5BMjM3MjQ1MzkxNl5BMl5BanBnXkFtZTgwODk1ODgyMjI@._V1_.jpg",
+        traileriframe:'https://youtu.be/Cz_-ak1TKpo'
+    },
+    {
+        id: 5,
+        title: "fast and furious",
+        rate: 4,
+        description:
+            "Fast and Furious — ou Rapides et Dangereux au Québec — est une série de films d'action américains, dont les thèmes tournent principalement autour des voitures et de la famille. Distribuée par Universal Pictures, elle comprend, à ce jour, neuf longs métrages et deux courts métrages..",
+        posterUrl:
+            "https://i.ytimg.com/vi/hCPXYelJteM/maxresdefault.jpg",
+        traileriframe:'https://youtu.be/XskpZiITrNI'
+    },
+    {
+        id: 6,
+        title: "tokyo ghoul",
+        rate: 4,
+        description:
+            "Tokyo Ghoul est un seinen manga de Sui Ishida prépublié dans le magazine Weekly Young Jump de l'éditeur Shūeisha. La première partie, Tokyo Ghoul, est parue de 2011 à 2014 et a été compilée en 14 volumes reliés. La deuxième partie, Tokyo Ghoul:re, est publiée entre 2014 et 2018 au Japon.",
+        posterUrl:
+            "https://m.media-amazon.com/images/M/MV5BN2E2OTgzODktMjBhYy00MjJjLWI0OTgtNGYxOGNhMWMxOWE4XkEyXkFqcGdeQXVyMzgxODM4NjM@._V1_.jpg",
+        traileriframe:'https://youtu.be/vGuQeQsoRgU'
+    }
+];
